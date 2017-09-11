2e1b28fd9c455f3b017336ba43966169bb0333f5
Laura-O <laura.ohrndorf@gmail.com>
2017-09-11 17:40:27 +0200

    fix: Add missing buttons and icons * Upvote and Downvote icon * Edit and delete button

--- src/components/PostEntry.js
+++ src/components/PostEntry.js
@@ -4,26 +4,55 @@ import {Button} from 'react-bootstrap';
 class PostEntry extends Component {
 	render() {
 		const {post} = this.props;
-		console.log(post)
+		console.log(post);
 		return (
-			<div className="card">
-				<div className="card-body">
-					<h2 className="card-title">
+			<div className="media">
+				<div className="media-vote">
+					<span>
+						<Button className="btn btn-score">
+							<span
+								className="glyphicon glyphicon-triangle-top"
+								aria-hidden="true"
+							/>
+						</Button>
+						<div className="vote-score">
+							{post.voteScore}
+						</div>
+						<Button className="btn btn-score">
+							<span
+								className="glyphicon glyphicon-triangle-bottom"
+								aria-hidden="true"
+							/>
+						</Button>
+					</span>
+				</div>
+				<div className="media-body">
+					<h2 className="media-heading">
 						{post.title}
 					</h2>
-					<p className="card-text">
+					<div>
 						{post.body}
-					</p>
-					<p className="card-text">
-						<small className="text-muted">
+					</div>
+					<p>
+						<Button bsSize="xsmall">
+							{post.category}
+						</Button>
+						<span>
 							Posted on {post.timestamp} by {post.author} in {post.category}
-						</small>
-					</p>					
+						</span>
+						<span>
+							<a href={'posts/' + post.id}>View</a>
+						</span>
+					</p>
+					<p>
+						<Button bsSize="xsmall">
+							Edit
+						</Button>
+						<Button bsSize="xsmall">
+							Delete
+						</Button>
+					</p>
 				</div>
-        <div className="card-footer">
-          <Button>{post.voteScore}</Button>
-          <a href={'posts/' + post.id}>View</a>
-        </div>
 			</div>
 		);
 	}
