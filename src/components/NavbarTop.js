0dd7441c5540fa0d27d143c372787c03d2d04742
Laura-O <laura.ohrndorf@gmail.com>
2017-09-25 19:44:39 +0200

    feat: add categories to navbar

--- src/components/NavbarTop.js
+++ src/components/NavbarTop.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import {Navbar, NavbarToggler, NavbarBrand} from 'reactstrap';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  NavItem,
+  Nav,
+  NavLink,
+} from 'reactstrap';
+import {fetchCategories} from '../actions';
 
-const NavbarTop = () => {
-	return (
-		<div>
-			<Navbar color="faded" light toggleable inverse>
-				<NavbarToggler right onClick={this.toggle} />
-				<NavbarBrand href="/">Readable</NavbarBrand>
-			</Navbar>
-		</div>
-	);
-};
+class NavbarTop extends Component {
+	componentWillMount() {
+		this.props.fetchCategories();
+	}
 
-export default NavbarTop;
+	renderCategories() {
+		const {categories} = this.props;
+		if (categories) {
+			return categories.map(category => (
+				<NavItem key={category.path}>
+					<NavLink key={category.path} className="text-capitalize" href={'/' + category.name}>{category.name}</NavLink>
+				</NavItem>
+			));
+		}
+	}
+
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light toggleable inverse>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">Readable</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+						{this.renderCategories()}
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return {categories: state.categories.all};
+}
+
+export default connect(mapStateToProps, {fetchCategories})(NavbarTop);
